0f60fb9fe236de9c63a9ccc2cc1f5ab43848b5a0
Priyanka-1Arora <98447746+Priyanka-1Arora@users.noreply.github.com>
2022-05-24 11:14:04 +0530

    Create project.cpp

---
+++ project.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+int main(){
+	// Adddition done by Priyanka
+	int a,b;
+	cout<<1.Addition;
+	cout<<endl;
+	cout<<"Enter Ist number";
+	cin>>a;
+	cout<<"Enter 2nd number";
+	cin>>b;
+	cout<<"After addition the result is: "<<(a+b);
+}
